86dbe7d29fcd28e648240eb29475678e9945894d
Bharath Muddada <muddadabharath@gmail.com>
2023-07-25 09:27:46 +0530

    added github actions yaml and updated hooks

--- tests/hooks/hooks.ts
+++ tests/hooks/hooks.ts
@@ -1,33 +1,41 @@
 import { After, AfterAll, Before, BeforeAll, Status } from "@cucumber/cucumber";
 import { Browser, BrowserContext, chromium } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
+const fs = require("fs-extra");
 
+let browser: Browser;
+let context: BrowserContext;
 
-let browser:Browser
-let context:BrowserContext
+BeforeAll(async function () {
+  browser = await chromium.launch({ headless: false });
+});
 
-BeforeAll(async function(){
+Before(async function () {
+  context = await browser.newContext({
+    recordVideo: { dir: "test-results/video" },
+  });
+  const page = await context.newPage();
+  pageFixture.page = page;
+});
 
-    browser = await chromium.launch({headless:false});
-
-})
-
-Before(async function(){
-
-   context = await browser.newContext()
-   const page = await context.newPage()
-   pageFixture.page = page
-})
-
-After(async function ({pickle,result}) {
-    if(result?.status == Status.FAILED){
-    const screenshotImg = await pageFixture.page.screenshot({path:`./test-results/screenshots/${pickle.name}`,type:"png"})
-    await this.attach(screenshotImg,"image/png")
-    }
-    await pageFixture.page.close()
-    await context.close();
-})
+After(async function ({ pickle, result }) {
+  let videoPath: string | undefined;
+  let screenshotImg: Buffer | any;
+  if (result?.status == Status.FAILED) {
+    screenshotImg = await pageFixture.page.screenshot({
+      path: `./test-results/screenshots/${pickle.name}`,
+      type: "png",
+    });
+    videoPath = await pageFixture.page.video()!.path();
+  }
+  await pageFixture.page.close();
+  await context.close();
+  if (result?.status == Status.FAILED) {
+    this.attach(screenshotImg, "image/png");
+    this.attach(fs.readFileSync(videoPath), "video/webm");
+  }
+});
 
 AfterAll(async function () {
-    await browser.close()
-})
+  await browser.close();
+});
